6f9df92568aa92b271355704376186fbd13139a8
MILLAN Romain <romainmillan@wabezeter.fr>
2022-04-06 22:56:36 +0200

    Récupération de l'emploi du temp S6 et ajout de tous les tuples dans une BD

--- BDSystem.php
+++ BDSystem.php
@@ -30,8 +30,48 @@ function counterExam(){
     return $rowcount;
 }
 
-function refreshEDT($grp){
+function refreshEDT(){
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     $con = mysqli_connect("localhost", "root", "", "myent");
 
+    $sqlRemoveTuple = "DELETE FROM edt WHERE groupeEDT='infoS6'";
+    mysqli_query($con, $sqlRemoveTuple);
 
+
+    //INFO 7
+    $url_file='https://proseconsult.umontpellier.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=58c99062bab31d256bee14356aca3f2423c0f022cb9660eba051b2653be722c4a5f10b982f9b914f8b3df9a16d82f493dc5c094f7d1a811b903031bde802c7f56c5ce5d7b8d9b880fb6990772f87c6e42988e4003796ffd7b370c710463ddfae894802120d56b2a4b539216dabc9724c7a8ae2f6a1bf0cab0beb54246d4caa04,1';
+    require_once('EDT/SG_iCal.php');
+    $ical = new SG_iCal($url_file);
+    foreach( $ical->getEvents() as $event ) {
+        //PROFESSEUR
+        $desc = $event->getDescription();
+        $array = array();
+        $desc= preg_replace("(\r\n|\n|\r)",' ',$desc);
+        $array = explode(' ', $desc);
+        $profEDT = "";
+        for($i=0; $i<count($array); $i++){
+            if(ctype_upper($array[$i]) == true && strlen($array[$i]) > 1){
+                $profEDT = $profEDT.$array[$i]." ";
+            }
+        }
+
+        //SQL
+        $nomEDT = $event->getSummary();
+        $groupeEDT = "infoS6";
+        $dateEDT = date('Y-m-d', $event->getStart());
+        $heureStart = date("H:m", $event->getStart());
+        $heureStop = date("H:m", $event->getEnd());
+        $descriptionEDT = $event->getDescription();
+        $salleEDT = $event->getLocation();
+
+        //INSERT INTO `edt` (`idEDT`, `nomEDT`, `groupeEDT`, `dateEDT`, `heureStart`, `heureStop`, `descriptionEDT`, `salleEDT`, `profEDT`) VALUES (NULL, 'test', 'test', '2022-04-13', '23:46', '23:50', 'fzf', 'fdz', 'vds');
+        $sql = "INSERT INTO `edt`(`idEDT`, `nomEDT`, `groupeEDT`, `dateEDT`, `heureStart`, `heureStop`, `descriptionEDT`, `salleEDT`, `profEDT`) VALUES(NULL, '$nomEDT', '$groupeEDT', '$dateEDT', '$heureStart', '$heureStop', '$descriptionEDT', '$salleEDT', '$profEDT')";
+        if(mysqli_query($con, $sql)){
+            echo("Succes");
+        }else {
+            echo("Error");
+        }
+    }
+
+    mysqli_close($con);
 }
--- testFile.php
+++ testFile.php
@@ -1,19 +1,18 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","myent");
+/*$url_file='https://proseconsult.umontpellier.fr/jsp/custom/modules/plannings/direct_cal.jsp?data=58c99062bab31d256bee14356aca3f2423c0f022cb9660eba051b2653be722c4a5f10b982f9b914f8b3df9a16d82f493dc5c094f7d1a811b903031bde802c7f56c5ce5d7b8d9b880fb6990772f87c6e42988e4003796ffd7b370c710463ddfae894802120d56b2a4b539216dabc9724c7a8ae2f6a1bf0cab0beb54246d4caa04,1';
 
-// SQL query to display row count
-// in building table
-$sql = "SELECT * from devoir";
+require_once('EDT/SG_iCal.php');
+$ical = new SG_iCal($url_file);
+echo '<pre>';
+foreach( $ical->getEvents() as $event ) {
+    //print_r( $event );
+    print(date('d/m/Y h:i:s', $event->getStart()));
+    print($event->getSummary());
+}*/
 
-if ($result = mysqli_query($con, $sql)) {
+include('BDSystem.php');
 
-    // Return the number of rows in result set
-    $rowcount = mysqli_num_rows( $result );
+refreshEDT();
 
-    // Display result
-    printf("Total rows in this table : %d\n", $rowcount);
-}
-
-// Close the connection
-mysqli_close($con);
+?>
